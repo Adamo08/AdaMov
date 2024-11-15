bcfe2a5b9283b6bd6dfcde36b438b94fddac0f3f
Adamo0 <you@example.com>
2024-11-15 14:24:00 +0100

    Updated login view

--- app/views/login.php
+++ app/views/login.php
@@ -1,13 +1,13 @@
     <?php require VIEWS."layouts/header.php"; ?>
 
     <!-- Normal Breadcrumb Begin -->
-    <section class="normal-breadcrumb set-bg" data-setbg="assets/images/normal-breadcrumb.jpg">
+    <section class="normal-breadcrumb set-bg" data-setbg="<?php echo IMAGES; ?>normal-breadcrumb.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="normal__breadcrumb__text">
                         <h2>Login</h2>
-                        <p>Welcome to the official Anime blog.</p>
+                        <p>Access your account and explore the world of movies!</p>
                     </div>
                 </div>
             </div>
@@ -21,6 +21,12 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="login__form">
+                        <!-- Show the verification success message -->
+                        <?php if (isset($verify_success)): ?>
+                            <div class="alert alert-success my-5">
+                                <?php echo $verify_success; unset($verify_success); ?>
+                            </div>
+                        <?php endif?>
                         <h3>Login</h3>
                         <form action="#">
                             <div class="input__item">
@@ -39,7 +45,7 @@
                 <div class="col-lg-6">
                     <div class="login__register">
                         <h3>Don't Have An Account?</h3>
-                        <a href="signup.html" class="primary-btn">Register Now</a>
+                        <a href="<?=url("auth/signup")?>" class="primary-btn">Register Now</a>
                     </div>
                 </div>
             </div>
