7fda54a542d4b6bebc6c039a70c4ed699847090b
Adamo0 <you@example.com>
2024-11-15 14:26:11 +0100

    Updated register view

--- app/views/register.php
+++ app/views/register.php
@@ -1,13 +1,13 @@
-    <?php require "./layouts/header.php"; ?>
+    <?php require VIEWS."layouts/header.php"; ?>
 
     <!-- Normal Breadcrumb Begin -->
-    <section class="normal-breadcrumb set-bg" data-setbg="assets/images/normal-breadcrumb.jpg">
+    <section class="normal-breadcrumb set-bg" data-setbg="<?php echo IMAGES; ?>normal-breadcrumb.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="normal__breadcrumb__text">
-                        <h2>Sign Up</h2>
-                        <p>Welcome to the official Anime blog.</p>
+                        <h2>Register</h2>
+                        <p>Create your account and join our movie-loving community!</p>
                     </div>
                 </div>
             </div>
@@ -19,25 +19,46 @@
     <section class="signup spad">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
+                <!-- Image Column -->
+                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                    <img src="https://via.placeholder.com/400x400" alt="Placeholder Image" class="img-fluid">
+                </div>
+                <!-- Signup Form Column -->
+                <div class="col-md-6">
                     <div class="login__form">
-                        <h3>Sign Up</h3>
-                        <form action="#">
-                            <div class="input__item ">
-                                <input class="col-md-12" type="text" placeholder="Email address">
-                                <span class="icon_mail"></span>
+                        <!-- Show the error message -->
+                        <?php if (isset($error)): ?>
+                            <div class="alert alert-danger my-5">
+                                <?php echo $error; unset($error); ?>
                             </div>
+                        <?php endif?>
+                        <!-- Show the success (verification email sent) message -->
+                        <?php if (isset($success)): ?>
+                            <div class="alert alert-success my-5">
+                                <?php echo $success; unset($success); ?>
+                            </div>
+                        <?php endif?>
+                        <h3>Sign Up</h3>
+                        <form action="<?= url("auth/register")?>" method="POST">
                             <div class="input__item">
-                                <input type="text" placeholder="Your Name">
+                                <input type="text" name="fname" placeholder="First Name">
                                 <span class="icon_profile"></span>
                             </div>
                             <div class="input__item">
-                                <input type="text" placeholder="Password">
+                                <input type="text" name="lname" placeholder="Last Name">
+                                <span class="icon_profile"></span>
+                            </div>
+                            <div class="input__item">
+                                <input class="col-md-12" type="text" name="email" placeholder="Email address">
+                                <span class="icon_mail"></span>
+                            </div>
+                            <div class="input__item">
+                                <input type="password" name="password" placeholder="Password">
                                 <span class="icon_lock"></span>
                             </div>
-                            <button type="submit" class="site-btn">Login Now</button>
+                            <button type="submit" class="site-btn">Sign Up</button>
                         </form>
-                        <h5>Already have an account? <a href="#">Log In!</a></h5>
+                        <h5>Already have an account? <a href="<?=url("auth/")?>">Log In!</a></h5>
                     </div>
                 </div>
             </div>
@@ -45,4 +66,4 @@
     </section>
     <!-- Signup Section End -->
 
-    <?php require "./layouts/footer.php"; ?>
+    <?php require VIEWS."layouts/footer.php"; ?>
