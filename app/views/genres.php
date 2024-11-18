81a919643bf6eb3984be0a547d6b5a5adadae719
Adamo0 <you@example.com>
2024-11-18 21:02:13 +0100

    Updated the genres view

--- app/views/genres.php
+++ app/views/genres.php
@@ -7,11 +7,17 @@
         $Movie = new Movie();
         $Review = new Review();
 
-        $genres = $Genre->all();
-        $movies = $Movie->all();
-        
+        $genres = isset(($category)) 
+                    ?
+                        (array) $Genre->getGenreByGenre(ucfirst($category))
+                    : 
+                        $Genre->all();
+
+        $currentCategory = $category ?? "All";
+        $categoryExists = !empty($genres);
     
     ?>
+
 
     <!-- Breadcrumb Begin -->
     <div class="breadcrumb-option">
@@ -19,9 +25,11 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb__links">
-                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
-                        <a href="./categories.html">Genres</a>
-                        <span>All</span>
+                        <a href="<?php echo url();?>"><i class="fa fa-home"></i> Home</a>
+                        <a href="<?php echo url("genres")?>">Genres</a>
+                        <span>
+                            <?php echo ucfirst($currentCategory); ?>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -36,59 +44,75 @@
 
                 <div class="col-lg-8">
 
-                    <!-- Genres section started -->
+                    <?php if ($categoryExists):?>
+                        <!-- Genres section started -->
+                        <?php foreach($genres as $genre) : ?>
+                            <div class="section__product mt-4">
 
-                    <?php foreach($genres as $genre) : ?>
-                        <div class="section__product mt-4">
-                            <div class="row">
-                                <div class="col-lg-8 col-md-8 col-sm-8">
-                                    <div class="section-title">
-                                        <h4><?= $genre['name']?> Movies</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-
-                                <?php foreach ($Movie->getMovies($genre['id']) as $movie) : ?>
-                                    <div class="col-lg-4 col-md-6 col-sm-6">
-                                        <div class="product__item">
-                                            <div class="product__item__pic set-bg" data-setbg="<?php echo ASSETS.$movie['thumbnail']?>">
-                                                <div class="ep">
-                                                    <?php for($i=0;$i<$Review->getTotalRating($movie['id']);$i++) : ?>
-                                                        <i class="fa fa-star"></i>
-                                                    <?php endfor; ?>
-                                                </div>
-                                                <div class="comment">
-                                                    <i class="fa fa-comments"></i>
-                                                    <?php echo $movie['comments_count']?>
-                                                </div>
-                                                <div class="view">
-                                                    <i class="fa fa-eye"></i>
-                                                    <?php echo $movie['views_count']?>
-                                                </div>
-                                            </div>
-                                            <div class="product__item__text">
-                                                <ul>
-                                                    <li class="bg-success">Active</li>
-                                                    <li>
-                                                        <?= $Genre->getName($movie['genre_id']) ?>
-                                                    </li>
-                                                </ul>
-                                                <h5>
-                                                    <a href="<?php echo url("movies/watch/".$movie['id'])?>">
-                                                        <?= $movie['title']?>
-                                                    </a>
-                                            </h5>
-                                            </div>
+                                <div class="row">
+                                    <div class="col-lg-8 col-md-8 col-sm-8">
+                                        <div class="section-title">
+                                            <h4><?= $genre['name']?> Movies</h4>
                                         </div>
                                     </div>
-                                <?php endforeach; ?>
+                                </div>
+                            
+                                <div class="row">
+
+                                    <?php 
+                                        // Get all movies for the current genre
+                                        $movies = $Movie->getMovies($genre['id']);
+                                        foreach ($movies as $movie) : 
+                                    ?>
+                                        <div class="col-lg-4 col-md-6 col-sm-6">
+                                            <div class="product__item">
+                                                <div class="product__item__pic set-bg" data-setbg="<?php echo ASSETS.$movie['thumbnail']?>">
+                                                    <div class="ep">
+                                                        <?php displayRating($movie['id']); ?>
+                                                    </div>
+                                                    <div class="comment">
+                                                        <i class="fa fa-comments"></i>
+                                                        <?php echo $movie['comments_count']?>
+                                                    </div>
+                                                    <div class="view">
+                                                        <i class="fa fa-eye"></i>
+                                                        <?php echo $movie['views_count']?>
+                                                    </div>
+                                                </div>
+                                                <div class="product__item__text">
+                                                    <ul>
+                                                        <li class="bg-success">Active</li>
+                                                        <li>
+                                                            <?= $Genre->getName($movie['genre_id']) ?>
+                                                        </li>
+                                                    </ul>
+                                                    <h5>
+                                                        <a href="<?php echo url("movies/watch/".$movie['id'])?>">
+                                                            <?= $movie['title']?>
+                                                        </a>
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
 
                             </div>
+                        <?php endforeach; ?>
+                        <!-- Genres section ended -->
+                    <?php else:?>
+                        <div class="section__product mt-4">
+                            <div class="alert alert-danger d-flex align-items-center" style="padding: 15px; border-radius: 5px;">
+                                <div style="margin-right: 10px; font-size: 24px;">
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                </div>
+                                <div>
+                                    <p style="margin: 0;">No movies found for the selected category. see <a href="<?php echo url("genres/")?>">All</a> instead</p>
+                                </div>
+                            </div>
                         </div>
-                    <?php endforeach; ?>
+                    <?php endif?>
 
-                    <!-- Genres section ended -->
 
                 </div>
 
@@ -103,7 +127,7 @@
                         </div>
                         <div class="product__sidebar__comment__item">
                             <div class="product__sidebar__comment__item__pic">
-                                <img src="assets/images/sidebar/comment-1.jpg" alt="">
+                                <img src="<?=IMAGES?>sidebar/comment-1.jpg" alt="">
                             </div>
                             <div class="product__sidebar__comment__item__text">
                                 <ul>
@@ -116,7 +140,7 @@
                         </div>
                         <div class="product__sidebar__comment__item">
                             <div class="product__sidebar__comment__item__pic">
-                                <img src="assets/images/sidebar/comment-2.jpg" alt="">
+                                <img src="<?=IMAGES?>sidebar/comment-2.jpg" alt="">
                             </div>
                             <div class="product__sidebar__comment__item__text">
                                 <ul>
@@ -129,7 +153,7 @@
                         </div>
                         <div class="product__sidebar__comment__item">
                             <div class="product__sidebar__comment__item__pic">
-                                <img src="assets/images/sidebar/comment-3.jpg" alt="">
+                                <img src="<?=IMAGES?>sidebar/comment-3.jpg" alt="">
                             </div>
                             <div class="product__sidebar__comment__item__text">
                                 <ul>
@@ -142,7 +166,7 @@
                         </div>
                         <div class="product__sidebar__comment__item">
                             <div class="product__sidebar__comment__item__pic">
-                                <img src="assets/images/sidebar/comment-4.jpg" alt="">
+                                <img src="<?=IMAGES?>sidebar/comment-4.jpg" alt="">
                             </div>
                             <div class="product__sidebar__comment__item__text">
                                 <ul>
@@ -160,8 +184,6 @@
         </div>
     </section>
     <!-- Genres Section End -->
-
-
 
     <?php require VIEWS."layouts/footer.php"; ?>
 
