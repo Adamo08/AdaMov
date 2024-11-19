113778ce8f7a251d5978462a8d59b5d634777dbc
Adamo0 <you@example.com>
2024-11-19 18:57:41 +0100

    Added the error message

--- app/views/error.php
+++ app/views/error.php
@@ -1,9 +1,17 @@
 
     <?php require VIEWS."layouts/header.php"; ?>
 
+    <?php 
+        $error_msg = isset($message) ? $message : 'An unknown error occurred.';
+    ?>
+
     <div class="mt-5 container-fluid error-container d-flex justify-content-center align-items-center">
         <div class="text-center p-5">
-            <h1 class="font-weight-bold text-white">Oops<span class="text-danger">!</span> Something went wrong <span class="text-danger">:(</span></h1>
+            <h2 class="font-weight-bold text-white">
+                Oops<span class="text-danger">!</span>
+                <?= $error_msg ?>
+                <span class="text-danger">:(</span>
+            </h2>
             <br>
             <img src="<?php echo IMAGES."error-image.png" ?>" class="img-fluid error-image" alt="Error Image" style="max-width: 300px;">
             <p class="lead mb-4">We're sorry, but the requested page wasn't found.</p>
