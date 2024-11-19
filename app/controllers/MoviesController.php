5f2f4a1a11d7ea4bdd0811271e08fd66a23c8804
Adamo0 <you@example.com>
2024-11-19 18:55:54 +0100

    Setting up the moviesController

---
+++ app/controllers/MoviesController.php
@@ -0,0 +1,52 @@
+<?php
+
+    class MoviesController extends Controller
+    {
+        /**
+         * Render the movie details view.
+         * 
+         * @param int $id The ID of the movie.
+         * @return void
+         */
+        public function show($id): void
+        {
+            // Validate if the passed ID
+            if (!is_numeric($id) || intval($id) <= 0) {
+                $this->view(
+                    'error',
+                    [
+                        'title' => 'Invalid ID',
+                        'message' => 'Invalid movie ID provided.'
+                    ]
+                );
+                return;
+            }
+        
+            $MovieModel = new Movie();
+            $GenresModel = new Genre();
+            $movie = $MovieModel->getMovieById((int)$id);
+        
+            if (!$movie) {
+                $this->view(
+                    'error',
+                    [
+                        'title' => 'Movie Not Found',
+                        'message' => 'The requested movie could not be found.'
+                    ]
+                );
+                return;
+            }
+        
+            // Render the movie details view.
+            $this->view(
+                'movie_details',
+                [
+                    'title' => $movie['title'],
+                    'movie' => $movie,
+                    'category' => $GenresModel->getName($MovieModel->getCategoryID($id))
+                ]
+            );
+        }
+
+
+    }
