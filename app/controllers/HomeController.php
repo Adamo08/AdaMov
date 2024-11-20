3f13cd3cb2a0c207689e30f7e8e2092e95825fd5
Adamo0 <you@example.com>
2024-11-20 22:11:47 +0100

    Rendering about and contact views

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,9 +4,28 @@
 
     class HomeController extends Controller
     {
+        /**
+         * Renders the home view.
+        */
         public function index()
         {
             $data = ["title"=>"Home Page"];
             $this->view('home',$data);
+        }
+
+        /**
+         * Render the about view
+        */
+        public function about(){
+            $data = ["title"=>"About Us"];
+            $this->view('about',$data);
+        }
+
+        /**
+         * The contact view
+        */
+        public function contact(){
+            $data = ["title"=>"Contact Us"];
+            $this->view('contact',$data);
         }
     }
