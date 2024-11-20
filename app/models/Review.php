c972fb7cb23896d5b52381a53be5f708f2c1d9f4
Adamo0 <you@example.com>
2024-11-20 22:14:15 +0100

    Fetching movie reviews

--- app/models/Review.php
+++ app/models/Review.php
@@ -31,5 +31,50 @@
             }
         }
 
+        /**
+         * Fetches all reviews for a particular movie.
+         * @param int $id movie ID
+         * @return array
+        */
+        public function getAllReviewsByMovie($id){
+            // Base query
+            $sql = "
+                    SELECT * FROM {$this->table} 
+                    WHERE media_id = :id
+                    ORDER BY created_at DESC
+                ";
+
+            // Prepare & Execute
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+            $stmt->execute();
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        
+        /**
+         * Returns the difference between the current date and the date of the review in seconds.
+         * @param int $id Review ID.
+         * @return int Difference in seconds.
+        */
+        public function getReviewPassesSeconds($id) {
+            // Base query
+            $sql = "
+                SELECT UNIX_TIMESTAMP(CURRENT_TIMESTAMP) - UNIX_TIMESTAMP(created_at) AS diff_in_seconds
+                FROM {$this->table}
+                WHERE id = :id
+            ";
+
+            // Prepare and execute
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            // Return the difference in seconds
+            return $stmt->fetchColumn();
+        }
+
+
 
     }
