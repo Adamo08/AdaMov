5b0d37fcd7d701f685e9d6f56cb63ba63f76018b
Adamo0 <you@example.com>
2024-11-27 00:21:53 +0100

    Added Adress field for ```users``` table

--- app/database/db.sql
+++ app/database/db.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
     fname VARCHAR(255) NOT NULL,
     lname VARCHAR(255) NOT NULL,
     avatar VARCHAR(255) DEFAULT NULL,
+    address VARCHAR(255) DEFAULT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     status BOOLEAN DEFAULT FALSE, -- Email verification status
