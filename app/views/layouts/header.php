ea4eca75bd4c71a560e54d78d2b71c61df5d99ff
Adamo0 <you@example.com>
2024-11-27 00:26:22 +0100

    Removed the ```session_start()``` from header.php to the ```index.php``` for global uses

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,18 +1,13 @@
 <?php 
 
     $isLoggedIn = false;
-    if (session_status() == PHP_SESSION_NONE){
-        
-        session_start();
 
-        if (isset($_SESSION['user_id'])){
-            $isLoggedIn = true;
-            $userId = $_SESSION['user_id'];
-            $UserModel = new User();
-            $full_name = $_SESSION['user_name'];
-            $avatar = $UserModel -> getAvatar($userId);
-        }
-
+    if (isset($_SESSION['user_id'])){
+        $isLoggedIn = true;
+        $userId = $_SESSION['user_id'];
+        $UserModel = new User();
+        $full_name = $_SESSION['user_name'];
+        $avatar = $UserModel -> getAvatar($userId);
     }
 
     $Genre = new Genre();
@@ -24,8 +19,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Anime Template">
-    <meta name="keywords" content="Anime, unica, creative, html">
+    <meta name="description" content="AdaMov Template">
+    <meta name="keywords" content="AdaMov, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
