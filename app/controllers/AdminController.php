e85bcb04f6e61d8e41eba9f81fdc5d87c992d3a6
Adamo08 <omarelaaddam519@gmail.com>
2024-11-30 20:01:25 +0100

    Setting up the admin panel

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php 
+
+    class AdminController extends Controller {
+
+        /**
+         * Shows admin dashboard and statistics (e.g., total movies, users, recent reviews).
+        */
+        public function index(){
+        }
+
+        /**
+         * Renders movies view
+        */
+        public function movies(){}
+
+        /**
+         * Renders genres view
+         */
+        public function genres(){}
+
+        /**
+         * Renders users view
+         */
+        public function users(){}
+
+        /**
+         * Renders admins view
+        */
+        public function admins(){}
+
+    }
