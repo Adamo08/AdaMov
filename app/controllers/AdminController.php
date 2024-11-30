18a2598dcb3baaec079d93c62912c4d5228e1b2c
Adamo08 <omarelaaddam519@gmail.com>
2024-11-30 23:01:35 +0100

    Added admin panel views for dashboard, login, forgot password, movies, genres, users, and admins

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -6,26 +6,49 @@
          * Shows admin dashboard and statistics (e.g., total movies, users, recent reviews).
         */
         public function index(){
+            $this->view('admin/dashboard');
+        }
+
+        /**
+         * Renders the login page
+        */
+        public function login(){
+            $this->view('admin/auth/login');
+        }
+
+        /**
+         * Renders The foget password view page
+        */
+        public function forgot_password(){
+            $this->view('admin/auth/forgot_password');
         }
 
         /**
          * Renders movies view
         */
-        public function movies(){}
+        public function movies(){
+            $this->view("admin/movies");
+        }
 
         /**
          * Renders genres view
          */
-        public function genres(){}
+        public function genres(){
+            $this->view("admin/genres");
+        }
 
         /**
          * Renders users view
          */
-        public function users(){}
+        public function users(){
+            $this->view("admin/users");
+        }
 
         /**
          * Renders admins view
         */
-        public function admins(){}
+        public function admins(){
+            $this->view("admin/admins");
+        }
 
     }
