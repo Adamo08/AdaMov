c89f07116fc4c504bcf2d35cbc1b06feee503a4a
Adamo08 <omarelaaddam519@gmail.com>
2024-12-03 22:16:38 +0100

    Listing movies

--- app/views/admin/movies.php
+++ app/views/admin/movies.php
@@ -8,11 +8,95 @@
             <h1 class="h3 mb-0 text-gray-800">Movies</h1>
         </div>
 
-        <!-- Content row  -->
-        <div class="row">
-            
-        </div>
+        <pre>
+            <?php //print_r($movies)?>
+        </pre>
 
+        <!-- Movies DataTales -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Movies DataTable</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>##</th>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Duration</th>
+                                <th>
+                                    <!-- Reviews icon -->
+                                    <i class="fas fa-comments"></i>
+                                </th>
+                                <th>
+                                    <!-- Views icon -->
+                                    <i class="fas fa-eye"></i>
+                                </th>
+                                <th>Status</th>
+                                <th>Release Date</th>
+                                <th>Added At</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>#</th>
+                                <th>##</th>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Duration</th>
+                                <th>
+                                    <!-- Reviews icon -->
+                                    <i class="fas fa-comments"></i>
+                                </th>
+                                <th>
+                                    <!-- Views icon -->
+                                    <i class="fas fa-eye"></i>
+                                </th>
+                                <th>Status</th>
+                                <th>Release Date</th>
+                                <th>Added At</th>
+                                <th>Actions</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <?php $i=0?>
+                            <?php foreach($movies as $movie):?>
+                                <tr>
+                                    <td><?php echo ++$i;?></td>
+                                    <td>
+                                        <img 
+                                            src="<?php echo ASSETS.$movie['thumbnail']?>"
+                                            alt="<?= $movie['title']?>"
+                                            style="width: 50px; height: 70px;"
+                                        >
+                                    </td>
+                                    <td><?= $movie['title']?></td>
+                                    <td><?= $movie['genre']?></td>
+                                    <td><?= formatMovieDuration($movie['duration'])?></td>
+                                    <td><?= $movie['comments_count']?></td>
+                                    <td><?= $movie['views_count']?></td>
+                                    <td><?= $movie['status']?></td>
+                                    <td><?= formatDate($movie['release_date'])?></td>
+                                    <td><?= formatDate($movie['created_at'])?></td>
+                                    <td>
+                                        <a href="" class="btn btn-danger btn-circle btn-sm">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                        <a href="" class="btn btn-primary btn-circle btn-sm">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endforeach?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php require ADMINVIEWS."layouts/footer.php"; ?>
