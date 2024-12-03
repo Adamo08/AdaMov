0faa304d11f88c0b7997b630a62750d67fb1ffb7
Adamo08 <omarelaaddam519@gmail.com>
2024-12-03 22:18:00 +0100

    Added datatables css links

--- app/views/admin/layouts/header.php
+++ app/views/admin/layouts/header.php
@@ -17,10 +17,14 @@
     <link href="<?php echo ADMINASSETS?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+        rel="stylesheet"
+    >
 
     <!-- Custom styles for this template-->
     <link href="<?php echo ADMINASSETS?>css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="<?php echo ADMINASSETS?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
