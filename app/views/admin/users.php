e6a43aa5c90c403ea51060db1edfd1f2584d5185
Adamo08 <omarelaaddam519@gmail.com>
2024-12-04 22:19:12 +0100

    Enhance user management UI with improved avatar handling, input validation, and modal design

--- app/views/admin/users.php
+++ app/views/admin/users.php
@@ -45,59 +45,75 @@
                             </tr>
                         </tfoot>
                         <tbody id="user-table-body">
-                            <?php $i=0?>
-                            <?php foreach($users as $user):?>
-                                <tr id="row_<?= $user['id']?>">
-                                    <td class="row-number"><?= ++$i?></td>
+                            <?php 
+                                $i = 0;
+                                $placeholder_img = ASSETS . "avatars/placeholder_avatar.png";
+                            ?>
+                            <?php foreach ($users as $user): ?>
+                                <tr id="row_<?= htmlspecialchars($user['id']) ?>">
+                                    <!-- Row Number -->
+                                    <td class="row-number"><?= ++$i ?></td>
+                                    
+                                    <!-- User Avatar -->
                                     <td>
                                         <img 
-                                            src="<?= ASSETS.$user['avatar']?>" 
-                                            alt="User Avatar" 
-                                            class="img-fluid"
+                                            src="<?= htmlspecialchars($user['avatar'] ? ASSETS . $user['avatar'] : $placeholder_img) ?>" 
+                                            alt="<?= htmlspecialchars($user['fname'] . " " . $user['lname']) ?>'s Avatar" 
+                                            class="img-fluid rounded-circle"
                                             style="width: 50px; height: 50px;"
                                         >
                                     </td>
+                                    
+                                    <!-- User First Name -->
+                                    <td><?= htmlspecialchars($user['fname']) ?></td>
+                                    
+                                    <!-- User Last Name -->
+                                    <td><?= htmlspecialchars($user['lname']) ?></td>
+                                    
+                                    <!-- User Address (with fallback and styling) -->
                                     <td>
-                                        <?= $user['fname']?>
+                                        <?= $user['address'] ?? '<span style="font-weight: bold; color: #ff0000;">N/A</span>' ?>
                                     </td>
+                                    
+                                    <!-- User Email -->
+                                    <td><?= htmlspecialchars($user['email']) ?></td>
+                                    
+                                    <!-- Account Creation Date -->
+                                    <td><?= formatDate($user['created_at']) ?></td>
+                                    
+                                    <!-- Action Buttons -->
                                     <td>
-                                        <?= $user['lname']?>
-                                    </td>
-                                    <td>
-                                        <?= $user['address']?>
-                                    </td>
-                                    <td>
-                                        <?= $user['email']?>
-                                    </td>
-                                    <td>
-                                        <?= formatDate($user['created_at'])?>
-                                    </td>
-                                    <td>
+                                        <!-- Delete Button -->
                                         <a 
-                                            href="" 
+                                            href="javascript:void(0);" 
                                             class="btn btn-danger btn-circle btn-sm remove_user_btn"
-                                            data-user-id="<?= $user['id']?>"
+                                            data-user-id="<?= htmlspecialchars($user['id']) ?>"
+                                            title="Delete User"
                                         >
                                             <i class="fas fa-trash"></i>
                                         </a>
+
+                                        <!-- Edit Button -->
                                         <a 
-                                            href="" 
+                                            href="javascript:void(0);" 
                                             class="btn btn-primary btn-circle btn-sm edit_user_btn" 
                                             data-toggle="modal" 
                                             data-target="#editUserModal" 
-                                            data-user-id="<?= $user['id'] ?>" 
-                                            data-fname="<?= $user['fname'] ?>" 
-                                            data-lname="<?= $user['lname'] ?>" 
-                                            data-address="<?= $user['address'] ?>" 
-                                            data-email="<?= $user['email'] ?>" 
-                                            data-avatar="<?= ASSETS . $user['avatar'] ?>"
+                                            data-user-id="<?= htmlspecialchars($user['id']) ?>" 
+                                            data-fname="<?= htmlspecialchars($user['fname']) ?>" 
+                                            data-lname="<?= htmlspecialchars($user['lname']) ?>" 
+                                            data-address="<?= htmlspecialchars($user['address'] ?? '') ?>" 
+                                            data-email="<?= htmlspecialchars($user['email']) ?>" 
+                                            data-avatar="<?= htmlspecialchars(ASSETS . $user['avatar'] ?? $placeholder_img) ?>"
+                                            title="Edit User"
                                         >
                                             <i class="fas fa-edit"></i>
                                         </a>
                                     </td>
                                 </tr>
-                            <?php endforeach?>
+                            <?php endforeach; ?>
                         </tbody>
+
                     </table>
                 </div>
             </div>
@@ -120,36 +136,42 @@
             <!-- Modal Body -->
             <div class="modal-body">
                 <form id="editUserForm">
-
                     <input type="hidden" id="edit_user_id" name="user_id">
 
                     <div class="row">
                         <!-- Avatar -->
                         <div class="col-md-4 text-center">
-                            <img id="edit_user_avatar" src="" alt="User Avatar" class="img-fluid rounded-circle mb-3" style="width: 150px; height: 150px;">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="edit_user_avatar_file" name="avatar">
-                                <label class="custom-file-label" for="edit_user_avatar_file">Choose new avatar</label>
+                            <div class="position-relative">
+                                <img 
+                                    id="edit_user_avatar" 
+                                    src="" 
+                                    alt="User Avatar" 
+                                    class="img-fluid rounded-circle mb-3 shadow"
+                                    data-toggle="tooltip" 
+                                    title="Only the user can update their avatar."
+                                >
                             </div>
                         </div>
 
                         <!-- User Details -->
                         <div class="col-md-8">
-                            <div class="form-group">
-                                <label for="edit_user_fname">First Name</label>
-                                <input type="text" class="form-control" id="edit_user_fname" name="fname" placeholder="Enter first name">
-                            </div>
-                            <div class="form-group">
-                                <label for="edit_user_lname">Last Name</label>
-                                <input type="text" class="form-control" id="edit_user_lname" name="lname" placeholder="Enter last name">
-                            </div>
-                            <div class="form-group">
-                                <label for="edit_user_address">Address</label>
-                                <input type="text" class="form-control" id="edit_user_address" name="address" placeholder="Enter address">
-                            </div>
-                            <div class="form-group">
-                                <label for="edit_user_email">Email Address</label>
-                                <input type="email" class="form-control" id="edit_user_email" name="email" placeholder="Enter email">
+                            <div class="p-3 border rounded bg-light">
+                                <div class="form-group">
+                                    <label for="edit_user_fname" class="font-weight-bold">First Name</label>
+                                    <input type="text" class="form-control" id="edit_user_fname" name="fname" placeholder="Enter first name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="edit_user_lname" class="font-weight-bold">Last Name</label>
+                                    <input type="text" class="form-control" id="edit_user_lname" name="lname" placeholder="Enter last name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="edit_user_address" class="font-weight-bold">Address</label>
+                                    <input type="text" class="form-control" id="edit_user_address" name="address" placeholder="Enter address">
+                                </div>
+                                <div class="form-group">
+                                    <label for="edit_user_email" class="font-weight-bold">Email Address</label>
+                                    <input type="email" class="form-control" id="edit_user_email" name="email" placeholder="Enter email">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -158,12 +180,17 @@
 
             <!-- Modal Footer -->
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary" id="saveChangesBtn">Save Changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                    <i class="fas fa-times"></i> Cancel
+                </button>
+                <button type="button" class="btn btn-success" id="saveChangesBtn">
+                    <i class="fas fa-save"></i> Save Changes
+                </button>
             </div>
         </div>
     </div>
 </div>
+
 
 
 <?php require ADMINVIEWS."layouts/footer.php"; ?>
