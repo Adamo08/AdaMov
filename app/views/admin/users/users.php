85971475b113bda6361eb12608effdc77e6da4f9
Adamo08 <omarelaaddam519@gmail.com>
2025-01-19 12:02:43 +0100

    Refactor edit user modal for improved readability and structure

--- app/views/admin/users/users.php
+++ app/views/admin/users/users.php
@@ -121,75 +121,75 @@
     </div>
 
 
-<!-- Edit User Modal -->
-<div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header bg-primary text-white">
-                <h5 class="modal-title" id="editUserModalLabel">Edit User Details</h5>
-                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+    <!-- Edit User Modal -->
+    <div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header bg-primary text-white">
+                    <h5 class="modal-title" id="editUserModalLabel">Edit User Details</h5>
+                    <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
 
-            <!-- Modal Body -->
-            <div class="modal-body">
-                <form id="editUserForm">
-                    <input type="hidden" id="edit_user_id" name="user_id">
+                <!-- Modal Body -->
+                <div class="modal-body">
+                    <form id="editUserForm">
+                        <input type="hidden" id="edit_user_id" name="user_id">
 
-                    <div class="row">
-                        <!-- Avatar -->
-                        <div class="col-md-4 text-center">
-                            <div class="position-relative">
-                                <img 
-                                    id="edit_user_avatar" 
-                                    src="" 
-                                    alt="User Avatar" 
-                                    class="img-fluid rounded-circle mb-3 shadow"
-                                    data-toggle="tooltip" 
-                                    title="Only the user can update their avatar."
-                                >
-                            </div>
-                        </div>
-
-                        <!-- User Details -->
-                        <div class="col-md-8">
-                            <div class="p-3 border rounded bg-light">
-                                <div class="form-group">
-                                    <label for="edit_user_fname" class="font-weight-bold">First Name</label>
-                                    <input type="text" class="form-control" id="edit_user_fname" name="fname" placeholder="Enter first name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="edit_user_lname" class="font-weight-bold">Last Name</label>
-                                    <input type="text" class="form-control" id="edit_user_lname" name="lname" placeholder="Enter last name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="edit_user_address" class="font-weight-bold">Address</label>
-                                    <input type="text" class="form-control" id="edit_user_address" name="address" placeholder="Enter address">
-                                </div>
-                                <div class="form-group">
-                                    <label for="edit_user_email" class="font-weight-bold">Email Address</label>
-                                    <input type="email" class="form-control" id="edit_user_email" name="email" placeholder="Enter email">
+                        <div class="row">
+                            <!-- Avatar -->
+                            <div class="col-md-4 text-center">
+                                <div class="position-relative">
+                                    <img 
+                                        id="edit_user_avatar" 
+                                        src="" 
+                                        alt="User Avatar" 
+                                        class="img-fluid rounded-circle mb-3 shadow"
+                                        data-toggle="tooltip" 
+                                        title="Only the user can update their avatar."
+                                    >
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
 
-            <!-- Modal Footer -->
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                    <i class="fas fa-times"></i> Cancel
-                </button>
-                <button type="button" class="btn btn-success" id="saveChangesBtn">
-                    <i class="fas fa-save"></i> Save Changes
-                </button>
+                            <!-- User Details -->
+                            <div class="col-md-8">
+                                <div class="p-3 border rounded bg-light">
+                                    <div class="form-group">
+                                        <label for="edit_user_fname" class="font-weight-bold">First Name</label>
+                                        <input type="text" class="form-control" id="edit_user_fname" name="fname" placeholder="Enter first name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="edit_user_lname" class="font-weight-bold">Last Name</label>
+                                        <input type="text" class="form-control" id="edit_user_lname" name="lname" placeholder="Enter last name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="edit_user_address" class="font-weight-bold">Address</label>
+                                        <input type="text" class="form-control" id="edit_user_address" name="address" placeholder="Enter address">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="edit_user_email" class="font-weight-bold">Email Address</label>
+                                        <input type="email" class="form-control" id="edit_user_email" name="email" placeholder="Enter email">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <!-- Modal Footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                        <i class="fas fa-times"></i> Cancel
+                    </button>
+                    <button type="button" class="btn btn-success" id="saveChangesBtn">
+                        <i class="fas fa-save"></i> Save Changes
+                    </button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
