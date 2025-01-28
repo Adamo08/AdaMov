3a2f542769702289cfaf392dbc188467402453ab
Adamo08 <omarelaaddam519@gmail.com>
2025-01-28 11:53:59 +0100

    Update add_movie form to remove required fields and adjust duration label

--- app/views/admin/movies/add_movie.php
+++ app/views/admin/movies/add_movie.php
@@ -19,23 +19,23 @@
                     <h6 class="m-0 font-weight-bold text-primary">Movie Details</h6>
                 </div>
                 <div class="card-body">
-                    <form id="addMovieForm" action="/admin/add_movie" method="POST" enctype="multipart/form-data">
+                    <form id="addMovieForm" method="POST" enctype="multipart/form-data">
                         <div class="form-group">
                             <label for="title">Movie Title</label>
-                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter movie title" required>
+                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter movie title" >
                         </div>
                         <div class="form-group">
                             <label for="description">Description</label>
-                            <textarea class="form-control" id="description" name="description" rows="3" placeholder="Enter movie description" required></textarea>
+                            <textarea class="form-control" id="description" name="description" rows="3" placeholder="Enter movie description" ></textarea>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="releaseDate">Release Date</label>
-                                <input type="date" class="form-control" id="releaseDate" name="release_date" required>
+                                <input type="date" class="form-control" id="releaseDate" name="release_date" >
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="genre">Genre</label>
-                                <select class="form-control" id="genre" name="genre" required>
+                                <select class="form-control" id="genre" name="genre" >
                                     <option value="" disabled selected>Select a genre</option>
                                     <?php foreach ($genres as $genre): ?>
                                         <option value="<?= htmlspecialchars($genre['id']) ?>">
@@ -48,25 +48,25 @@
                         <div class="form-group">
                             <label for="thumbnail">Thumbnail (Image)</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="thumbnail" name="thumbnail" accept="image/*" required>
+                                <input type="file" class="custom-file-input" id="thumbnail" name="thumbnail" accept="image/*" >
                                 <label class="custom-file-label" for="thumbnail">Choose thumbnail</label>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="fileName">Movie File (MP4)</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="fileName" name="file_name" accept="video/mp4" required>
+                                <input type="file" class="custom-file-input" id="fileName" name="file_name" accept="video/mp4" >
                                 <label class="custom-file-label" for="fileName">Choose movie file</label>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="duration">Duration</label>
-                                <input type="text" class="form-control" id="duration" name="duration" placeholder="e.g., 2h 15m" required>
+                                <label for="duration">Duration In Seconds</label>
+                                <input type="text" class="form-control" id="duration" name="duration" placeholder="e.g., 4200" >
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="quality">Quality</label>
-                                <select class="form-control" id="quality" name="quality" required>
+                                <select class="form-control" id="quality" name="quality" >
                                     <option value="" disabled selected>Choose quality</option>
                                     <option value="1080p">1080p</option>
                                     <option value="720p">720p</option>
@@ -76,7 +76,7 @@
                             </div>
                         </div>
                         <div class="form-group text-center">
-                            <button type="submit" class="btn btn-primary btn-block">Add Movie</button>
+                            <button type="submit" class="btn btn-primary btn-block" id="addMovieButton">Add Movie</button>
                         </div>
                     </form>
                 </div>
